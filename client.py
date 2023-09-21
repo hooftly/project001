03095052fc535e1758ce37d2c6302a07ce5825c6
hooftly <mhooft@tf-dev.org>
2023-09-21 11:11:16 -0600

    Change User handling and add basic data handling

--- client.py
+++ client.py
@@ -13,36 +13,75 @@ def start_client():
         client_socket.connect(server_address)
 
         # Receive and print the welcome message
-        welcome_message = client_socket.recv(1024).decode()
-        print(welcome_message)
+        welcome_message_code = client_socket.recv(1)
+        print("Welcome to the server!")
 
-        while True:
-            # Prompt for registration or login
-            action = input().strip()
-            client_socket.sendall(action.encode())
+        # Prompt for registration or login
+        action = input("Register or Login? (Type '1' for register, '2' for login, 'q' to quit): ").strip()
+        client_socket.sendall(bytes([int(action)]))
 
-            response = client_socket.recv(1024).decode()
-            print(response)
+        if action == '1':
+            response_code = client_socket.recv(1)
 
-            if response.startswith('Enter username'):
+            if response_code == b'\x01':
+                # Register a new user
+                print("Enter username:")
                 username = input().strip()
                 client_socket.sendall(username.encode())
 
+                print("Enter password:")
                 password = input().strip()
                 client_socket.sendall(password.encode())
 
-                registration_response = client_socket.recv(1024).decode()
-                print(registration_response)
+                registration_response_code = client_socket.recv(1)
 
-            elif response.startswith('Enter username'):
+                if registration_response_code == b'\x01':
+                    print("Registration successful! You are now logged in.")
+                    print("Enter a message to send to the server (or type 'exit' to quit): ")
+
+                    while True:
+                        message = input().strip()
+                        client_socket.sendall(message.encode())
+
+                        if message == 'exit':
+                            break
+                else:
+                    print("Registration failed. Please try again.")
+            else:
+                print("Server did not respond properly to registration request.")
+
+        elif action == '2':
+            response_code = client_socket.recv(1)
+
+            if response_code == b'\x01':
+                # Login an existing user
+                print("Enter username:")
                 username = input().strip()
                 client_socket.sendall(username.encode())
 
+                print("Enter password:")
                 password = input().strip()
                 client_socket.sendall(password.encode())
 
-                login_response = client_socket.recv(1024).decode()
-                print(login_response)
+                login_response_code = client_socket.recv(1)
+
+                if login_response_code == b'\x01':
+                    print(f"Welcome, {username}! You are logged in.")
+                    print("Enter a message to send to the server (or type 'exit' to quit): ")
+
+                    while True:
+                        message = input().strip()
+                        client_socket.sendall(message.encode())
+
+                        if message == 'exit':
+                            break
+                else:
+                    print("Invalid username or password. Please try again.")
+            else:
+                print("Server did not respond properly to login request.")
+
+        elif action == 'q':
+            pass
 
     finally:
         # Close the client socket
--- server.py
+++ server.py
@@ -20,42 +20,59 @@ def handle_client(client_socket, client_address):
     print(f'Connection from {client_address}')
 
     # Send a welcome message and prompt for registration
-    client_socket.sendall('Welcome to the server!'.encode())
-    client_socket.sendall('Register or Login? (Type "register" or "login"): '.encode())
+    client_socket.sendall(b'\x01')  # 0x01 indicates welcome message
+    client_socket.sendall(b'\x01')  # 0x01 indicates registration/login prompt
 
     while running:
-        # Receive data from the client
-        data = client_socket.recv(1024).decode().strip()
+        try:
+            # Receive data from the client
+            message_type = client_socket.recv(1)
 
-        if not data:
-            break
+            if message_type == b'\x01':
+                # Register a new user
+                client_socket.sendall(b'\x01')  # 0x01 indicates enter username
+                username = client_socket.recv(1024).decode().strip()
 
-        if data.lower() == 'register':
-            # Register a new user
-            client_socket.sendall('Enter username: '.encode())
-            username = client_socket.recv(1024).decode().strip()
+                client_socket.sendall(b'\x01')  # 0x01 indicates enter password
+                password = client_socket.recv(1024).decode().strip()
 
-            client_socket.sendall('Enter password: '.encode())
-            password = client_socket.recv(1024).decode().strip()
+                user_accounts[username] = User(username, password)
+                client_socket.sendall(b'\x01')  # 0x01 indicates registration successful
 
-            user_accounts[username] = User(username, password)
-            client_socket.sendall(f'Registration successful! You are now logged in as {username}.'.encode())
+                # Start processing messages from the client after successful registration
+                while True:
+                    message = client_socket.recv(1024)
+                    if not message:
+                        break
+                    print(f'Received from {username}: {message.decode()}')
 
-        elif data.lower() == 'login':
-            # Login an existing user
-            client_socket.sendall('Enter username: '.encode())
-            username = client_socket.recv(1024).decode().strip()
+            elif message_type == b'\x02':
+                # Login an existing user
+                client_socket.sendall(b'\x01')  # 0x01 indicates enter username
+                username = client_socket.recv(1024).decode().strip()
 
-            client_socket.sendall('Enter password: '.encode())
-            password = client_socket.recv(1024).decode().strip()
+                client_socket.sendall(b'\x01')  # 0x01 indicates enter password
+                password = client_socket.recv(1024).decode().strip()
 
-            if username in user_accounts and user_accounts[username].password == password:
-                client_socket.sendall(f'Welcome, {username}! You are logged in.'.encode())
+                if username in user_accounts and user_accounts[username].password == password:
+                    client_socket.sendall(b'\x01')  # 0x01 indicates successful login
+
+                    while True:
+                        # Send data in chunks of 1024 bytes after successful login
+                        message = client_socket.recv(1024)
+                        if not message:
+                            break
+                        print(f'Received from {username}: {message.decode()}')
+
+                else:
+                    client_socket.sendall(b'\x00')  # 0x00 indicates failed login
+
             else:
-                client_socket.sendall('Invalid username or password. Please try again.'.encode())
+                client_socket.sendall(b'\x00')  # 0x00 indicates invalid input
 
-        else:
-            client_socket.sendall('Invalid input. Type "register" or "login": '.encode())
+        except ConnectionResetError:
+            print(f'Connection with {client_address} reset by peer.')
+            break
 
     # Close the client socket
     client_socket.close()
